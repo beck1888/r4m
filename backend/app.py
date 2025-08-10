002137cc80376c47f932873b3cf795112b88f123
Beck Orion <beckorion@gmail.com>
2025-08-10 16:22:02 -0700

    add transcript fetching function

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 ## Importing libs
 # Project imports
 from backend_helpers.urls import extract_youtube_id
+from backend_helpers.youtube import fetch_transcript
 # External dependencies
 from flask import Flask, request, jsonify # Web Server
 
@@ -11,15 +12,43 @@ app = Flask(__name__)
 # Endpoint for getting a video's ID
 @app.get("/get-video-id")
 def get_video_id():
+    # Parse request for url param
     url = request.args.get("url", type=str)
+    # Error handling if no url is given
     if not url:
         return jsonify(error="Missing 'url' query parameter"), 400
 
-    vid = extract_youtube_id(url)
-    if not vid:
+    # Get the video id
+    vid_id = extract_youtube_id(url)
+
+    # Error handling if the url was bad
+    if not vid_id:
         return jsonify(error="Unsupported or unrecognized YouTube URL", url=url), 422
 
-    return jsonify(video_id=vid, url=url), 200
+    # Success, return the ID
+    return jsonify(video_id=vid_id, url=url), 200
+
+# Endpoint for getting a video's transcript
+@app.get("/fetch-transcript")
+def fetch_youtube_video_transcript():
+    # Parse the url for the id param
+    id = request.args.get("id", type=str)
+    # No id given error handling
+    if not id:
+        return jsonify(error="Missing 'id' query parameter"), 400
+
+    # Do the request
+    transcript = fetch_transcript(id)
+
+    # Error handling for blank or TranscriptDisabled responses
+    if transcript is None: # Equivalent to TranscriptDisabled error from extraction function
+        return jsonify(error="Video does not have a transcript", id=id), 404
+    
+    if len(transcript) < 1: # Blank transcript
+        return jsonify(error="Transcript is blank", id=id), 404
+    
+    # Success, return transcript
+    return jsonify(transcript=transcript), 200
 
 ## Run the dev server
 if __name__ == "__main__":
