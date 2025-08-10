2af24f4042109c35e39c8b45072243843e811b9b
Beck Orion <beckorion@gmail.com>
2025-08-10 16:01:21 -0700

    add transcript error handling for no transcript

--- backend/backend_helpers/youtube.py
+++ backend/backend_helpers/youtube.py
@@ -1,16 +1,26 @@
 ## Imports
-from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 
 ## Functions
 def fetch_transcript(video_id: str) -> str:
-    transcript_fetcher = YouTubeTranscriptApi()
-    fetched = transcript_fetcher.fetch(video_id)
+    transcript_fetcher = YouTubeTranscriptApi() # Create fetcher
+
+    # Try to fetch a transcript
+    try:
+        fetched = transcript_fetcher.fetch(video_id)
+    except TranscriptsDisabled:
+        return None # No transcript available
+    
+    # If there is a transcript, re-assemble it into one string
     transcript = " ".join(snippet.text for snippet in fetched) # Extract just the string parts
     return transcript
 
 ## Testing it
 def main():
-    t = fetch_transcript('fHRS_NOs24w')
+    actual_video_with_transcript = 'fHRS_NOs24w'
+    video_without_transcript = 'hvD8t7uzD7k'
+
+    t = fetch_transcript(video_without_transcript)
     print(t)
 
 if __name__ == '__main__':
