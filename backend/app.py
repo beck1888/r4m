13c7f4f8817172a2e8e2bc2ad9f17dc462cb0335
Beck Orion <beckorion@gmail.com>
2025-08-10 15:28:10 -0700

    test out route with a parameter

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,20 @@
-# Importing libs
+## Importing libs
 from flask import Flask # Web Server
 
 # Create flask app
 app = Flask(__name__)
 
-# App Routes
+## App Routes
+# Default route
 @app.route("/")
 def home():
     return "Hello, World!"
 
-# Run the dev server
+# Route with parameter
+@app.route("/greet/<name>") 
+def hello(name):
+    return f"Hello, {name}!"
+
+## Run the dev server
 if __name__ == "__main__":
     app.run(debug=True)
